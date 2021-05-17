ce1151a9ed50e2c72f1d05e72a7e101b2ba8f596
Kathryn Dolby <kathryndolby6@gmail.com>
2021-05-17 21:49:54 +0000

    added primary key (start/end ip's) to sql schema

--- geoip-helper/check_mmdb.py
+++ geoip-helper/check_mmdb.py
@@ -2,11 +2,11 @@
 
 import geoip2.database
 
-ip = '2001:4860:4860::8888' #'62.18.255.1'
-with geoip2.database.Reader('./dbip.mmdb') as reader:
+ip = '2001:4860:4860::8888'
+with geoip2.database.Reader('./dbip-city-lite-2021-05.mmdb') as reader:
     response = reader.city(ip)
 
-    print(response.country.name,
+    print(response.continent.code, response.continent.name, response.country.iso_code, response.country.name,
     response.city.name,
     response.subdivisions.most_specific.name,
     response.location.latitude,
--- geoip-helper/create-geoip.sql
+++ geoip-helper/create-geoip.sql
@@ -1,8 +1,20 @@
 drop table if exists geoip;
 
-create table geoip (start_ip varchar(255), end_ip varchar(255), continent_code varchar(255), continent varchar(255), country_code varchar(255), country varchar(255), city varchar(255), region varchar(255), latitude double, longitude double);
+create table geoip (
+	start_ip decimal(39,0) not null, 
+	end_ip decimal(39,0) not null, 
+	continent_code varchar(255), 
+	continent varchar(255), 
+	country_code varchar(255), 
+	country varchar(255), 
+	city varchar(255), 
+	region varchar(255), 
+	latitude double not null, 
+	longitude double not null,
+	primary key (start_ip, end_ip)
+);
 
-load data local infile '/root/heprc/production-helpers/geoip-helper/geoip_db.csv'
+load data local infile '/root/production-helpers/geoip-helper/geoip_db.csv'
 into table geoip
 fields terminated by ','
 lines terminated by '\n'
