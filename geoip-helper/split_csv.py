ecbf83a3a22c820af208eb35fde89f256e497c60
Kathryn Dolby <kathryndolby6@gmail.com>
2021-05-19 22:53:58 +0000

    created split_csv.py to separate ipv4 and ipv6 addresses

---
+++ geoip-helper/split_csv.py
@@ -0,0 +1,20 @@
+import sys
+import pandas as pd
+
+filename = sys.argv[1]
+df = pd.read_csv(filename, chunksize=10000)
+
+write_header = True
+
+for chunk in df:
+   ipv4_rows = []
+   ipv6_rows = []
+   for index, row in chunk.iterrows():
+      if int(row['start_ip']) < 4294967296:
+         ipv4_rows.append(row)
+      else:
+         ipv6_rows.append(row)
+   pd.DataFrame(ipv4_rows).to_csv('./ipv4.csv', mode='a', header=write_header, index=False)
+   pd.DataFrame(ipv6_rows).to_csv('./ipv6.csv', mode='a', header=write_header, index=False)
+   write_header = False
+   print('.', end='')
