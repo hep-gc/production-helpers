053ee4b550c150acf3104ad9659d7ec161813d97
Kathryn Dolby <kathryndolby6@gmail.com>
2021-05-20 20:45:58 +0000

    updated create-geoip.sql to be fastest db schema for queries

--- geoip-helper/README.md
+++ geoip-helper/README.md
@@ -1,11 +1,15 @@
 Start with conversion of mmdb file to csv. Change path/file names within mmdb_to_csv.py as needed.
 
 For create-geoip.sql, must load a csv file of geodata in the following format of 10 columns:
-start ip, end ip, continent code, continent, country code, country, city, region, latitude, longitude
-Where the start/end IPs are in integer format.
+* start ip, end ip, continent code, continent, country code, country, city, region, latitude, longitude
+(Where the start/end IPs are in integer format.)
 
 Using MariaDB, with an existing database created, load in the sql file:
-mysql <database_name> < create-geoip.sql
+`mysql <database_name> < create-geoip.sql`
+
 Query this database with an IP address:
-select * from <database_name> where start_ip <= ip_int and end_ip >= ip_int;
-Where ip_int is the IP address in integer format.
+* `select * from ipv4 where end_ip >= x order by end_ip asc limit 1;`
+(Where x is the IPv4 address.)
+
+* `select * from ipv6 where end_ip >= y order by end_ip asc limit 1;`
+(Where y is the IPv6 address in integer format.)
--- geoip-helper/create-geoip.sql
+++ geoip-helper/create-geoip.sql
@@ -1,21 +1,5 @@
--- drop table if exists geoip;
 drop table if exists ipv4;
--- drop table if exists ipv6;
-
-/*
-create table geoip (
-	start_ip decimal(39,0) not null, 
-	end_ip decimal(39,0) not null, 
-	continent_code varchar(255), 
-	continent varchar(255), 
-	country_code varchar(255), 
-	country varchar(255), 
-	city varchar(255), 
-	region varchar(255), 
-	latitude double not null, 
-	longitude double not null
-);
-*/
+drop table if exists ipv6;
 
 create table ipv4 (
 	start_ip int unsigned  not null, 
@@ -29,7 +13,7 @@ create table ipv4 (
 	latitude double not null, 
 	longitude double not null
 );
--- primary key(start_ip, end_ip)
+alter table ipv4 engine = MyISAM;
 
 load data local infile './ipv4.csv'
 into table ipv4
@@ -38,22 +22,8 @@ lines terminated by '\n'
 ignore 1 rows
 (start_ip, end_ip, continent_code, continent, country_code, country, city, region, latitude, longitude);
 
-create index start on ipv4(start_ip);
-create index end on ipv4(end_ip);
-/*
-create table ipv4 (
-	start_ip int unsigned  not null, 
-	end_ip int unsigned not null, 
-	continent_code varchar(255), 
-	continent varchar(255), 
-	country_code varchar(255), 
-	country varchar(255), 
-	city varchar(255), 
-	region varchar(255), 
-	latitude double not null, 
-	longitude double not null,
-	primary key (start_ip, end_ip)
-);
+create index end4 on ipv4(end_ip);
+
 create table ipv6 (
 	start_ip decimal(39,0) not null, 
 	end_ip decimal(39,0) not null, 
@@ -67,9 +37,13 @@ create table ipv6 (
 	longitude double not null,
 	primary key (start_ip, end_ip)
 );
+alter table ipv6 engine = MyISAM;
 
-insert into ipv4 select * from geoip where start_ip < 4294967296;
-insert into ipv6 select * from geoip where start_ip >= 4294967296;
+load data local infile './ipv6.csv'
+into table ipv6
+fields terminated by ','
+lines terminated by '\n'
+ignore 1 rows
+(start_ip, end_ip, continent_code, continent, country_code, country, city, region, latitude, longitude);
 
-drop table if exists geoip;
-*/
+create index end6 on ipv6(end_ip);
