db557cccacd769bc97d266d9d43624fcaad14b92
Kathryn Dolby <kathryndolby6@gmail.com>
2021-05-25 18:17:27 +0000

    added ability to test ipv6 or ipv4 in test_sql.py

--- geoip-helper/test_sql.py
+++ geoip-helper/test_sql.py
@@ -11,21 +11,18 @@ mydb = mysql.connector.connect(
    database="test_db"
 )
 
-#with mydb.cursor(dictionary=True) as cursor:
-cursor = mydb.cursor(dictionary=True)
 address = ipaddress.ip_address(sys.argv[1])
 ip = int(address)
-#if address.version == 4:
-#   table = ipv4
-#else:
-#   table = ipv6
+if address.version == 4:
+   table = 'ipv4'
+else:
+   table = 'ipv6'
 
-select = "SELECT * FROM ipv4 WHERE end_ip >= INET_ATON(%s) ORDER BY end_ip ASC LIMIT 1"
-cursor.execute("SELECT * FROM ipv4 WHERE end_ip >= %d ORDER BY end_ip ASC LIMIT 1" % ip)
+cursor = mydb.cursor(dictionary=True)
+cursor.execute("SELECT * FROM %s WHERE end_ip >= %d ORDER BY end_ip ASC LIMIT 1" % (table, ip))
 
 row = cursor.fetchone()
-#print(ip,row)
 if row is None or row['start_ip'] > ip:
-   print("IP address is not database")
+   print("IP address is not in database")
 else:
    print(row)
